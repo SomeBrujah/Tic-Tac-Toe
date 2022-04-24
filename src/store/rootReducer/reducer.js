58cfdfcbfb0a42969d8278ae7e70e6741ca754e5
SomeBrujah <wissenseeker@gmail.com>
2022-04-24 22:58:02 +0300

    Removed comments

--- src/store/rootReducer/reducer.js
+++ src/store/rootReducer/reducer.js
@@ -8,45 +8,6 @@ const defaultState = {
     winner: null
 }
 
-// const rootReducer = (state = defaultState, { type, payload }) => {
-//     switch (type) {
-//         case HANDLE_CELL_CLICK:
-//             const newState = {
-//                 cells: state.cells,
-//                 isX: state.isX,
-//                 winner: state.winner
-//             }
-//             if (newState.winner !== null || newState.cells[payload] !== null) {
-//                 return state;
-//             }
-//             newState.cells[payload] = newState.isX ? 'X' : 'O';
-//             newState.isX = !newState.isX;
-//             let result = calculateWinerComb(newState.cells);
-//             if (result === null && !newState.cells.includes(null)) {
-//                 newState.winner = 'Draw';
-//             } else if (result !== null) {
-//                 newState.winner = `Winner is: ${result}`;
-//             }
-//             return {
-//                 ...state,
-//                 cells: newState.cells,
-//                 isX: newState.isX,
-//                 winner: newState.winner
-//             };
-
-//         case RESTART_GAME:
-//             return {
-//                 ...state,
-//                 cells: state.cells = new Array(9).fill(null),
-//                 isX: state.isX = true,
-//                 winner: state.winner = null
-//             }
-
-//         default:
-//             return state;
-//     }
-// }
-
 const rootReducer = handleActions({
     [HANDLE_CELL_CLICK]: (state, { payload }) => {
         const newState = {
